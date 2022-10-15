6cff80669d9f28771ba23aa2b0a3cdec1b5ce4a8
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2022-10-15 23:56:38 +0400

    product page

---
+++ src/pages/Product.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+
+const Container = styled.div``;
+
+const Product = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Wrapper>
+        <ImgContainer>
+          <Image src="https://i.ibb.co/S6qMxwr/jean.jpg" />
+        </ImgContainer>
+      </Wrapper>
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
+
+export default Product;
