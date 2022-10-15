a219268fdee61f3d7ad9f4957bea1eccd97d2eb0
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2022-10-16 02:52:04 +0400

    single product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 import styled from "styled-components";
 import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
@@ -6,6 +7,96 @@ import Navbar from "../components/Navbar";
 import Newsletter from "../components/Newsletter";
 
 const Container = styled.div``;
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`;
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+`;
+const Title = styled.h1`
+  font-weight: 200;
+`;
+const Desc = styled.p`
+  margin: 20px 0;
+`;
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+  width: 50%;
+  display: flex;
+  justify-content: space-between;
+  margin: 30px 0;
+`;
+
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`;
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${({ color }) => color};
+  margin: 0 5px;
+  cursor: pointer;
+`;
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`;
+const FilterSizeOption = styled.option``;
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+  font-size: 24px;
+`;
+const Amount = styled.span`
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid teal;
+  font-size: 18px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 5px;
+`;
+const Button = styled.button`
+  padding: 15px;
+  border: 2px solid teal;
+  background-color: #fff;
+  cursor: pointer;
+  font-weight: 500;
+  transition: all 0.5s ease;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.2);
+  }
+`;
 
 const Product = () => {
   return (
@@ -16,6 +107,42 @@ const Product = () => {
         <ImgContainer>
           <Image src="https://i.ibb.co/S6qMxwr/jean.jpg" />
         </ImgContainer>
+        <InfoContainer>
+          <Title>Denim Jumpsuit</Title>
+          <Desc>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil quos
+            nulla libero perferendis mollitia dolore eligendi quas impedit
+            cupiditate, totam, sapiente facilis possimus, animi laudantium
+            quidem ut modi quisquam quis.
+          </Desc>
+          <Price>$ 20</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+              <AiOutlineMinus />
+              <Amount>1</Amount>
+              <AiOutlinePlus />
+            </AmountContainer>
+            <Button>ADD TO CART</Button>
+          </AddContainer>
+        </InfoContainer>
       </Wrapper>
       <Newsletter />
       <Footer />
