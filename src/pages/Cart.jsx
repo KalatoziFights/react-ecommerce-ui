abeb7e3ae1a55d7d4292374fffa1906130fa88f4
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2022-10-19 02:34:00 +0400

    product details

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -19,7 +19,6 @@ const Top = styled.div`
   justify-content: space-between;
   padding: 20px;
 `;
-
 const TopButton = styled.button`
   padding: 10px;
   font-weight: 600;
@@ -36,7 +35,70 @@ const TopText = styled.span`
   cursor: pointer;
   margin: 0px 10px;
 `;
-const Bottom = styled.div``;
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const Info = styled.div`
+  flex: 3;
+`;
+const Summary = styled.div`
+  flex: 1;
+`;
+
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const ProductDetail = styled.div`
+  display: flex;
+  flex: 2;
+`;
+const Image = styled.img`
+  width: 200px;
+`;
+const Details = styled.span`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+const ProductName = styled.span``;
+const ProductId = styled.span``;
+const ProductColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${({ color }) => color};
+`;
+const ProductSize = styled.span``;
+const PriceDetail = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+const ProductAmount = styled.div`
+  font-size: 24px;
+  margin: 5px;
+`;
+const ProductPrice = styled.div`
+  font-size: 30px;
+  font-weight: 200;
+`;
+
+const HR = styled.hr`
+  background-color: #eee;
+  border: none;
+  height: 1px;
+`;
 
 const Cart = () => {
   return (
@@ -53,7 +115,91 @@ const Cart = () => {
           </TopTexts>
           <TopButton type="filled">CHECKOUT NOW</TopButton>
         </Top>
-        <Bottom>34</Bottom>
+        <Bottom>
+          <Info>
+            <Product>
+              <ProductDetail>
+                <Image src="https://cdn-images.farfetch-contents.com/12/98/05/13/12980513_15990369_1000.jpg" />
+                <Details>
+                  <ProductName>
+                    <b>Product:</b> JESSIE THUNDER SHOES
+                  </ProductName>
+                  <ProductId>
+                    <b>ID:</b> 93813718293
+                  </ProductId>
+                  <ProductColor color="black" />
+                  <ProductSize>
+                    <b>Size:</b>
+                    37.5
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  add
+                  <ProductAmount>2</ProductAmount>
+                  remove
+                </ProductAmountContainer>
+                <ProductPrice>$ 30</ProductPrice>
+              </PriceDetail>
+            </Product>
+            <HR />
+            <Product>
+              <ProductDetail>
+                <Image src="https://cdn-images.farfetch-contents.com/12/98/05/13/12980513_15990369_1000.jpg" />
+                <Details>
+                  <ProductName>
+                    <b>Product:</b> JESSIE THUNDER SHOES
+                  </ProductName>
+                  <ProductId>
+                    <b>ID:</b> 93813718293
+                  </ProductId>
+                  <ProductColor color="black" />
+                  <ProductSize>
+                    <b>Size:</b>
+                    37.5
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  add
+                  <ProductAmount>2</ProductAmount>
+                  remove
+                </ProductAmountContainer>
+                <ProductPrice>$ 30</ProductPrice>
+              </PriceDetail>
+            </Product>
+            <HR />
+            <Product>
+              <ProductDetail>
+                <Image src="https://cdn-images.farfetch-contents.com/12/98/05/13/12980513_15990369_1000.jpg" />
+                <Details>
+                  <ProductName>
+                    <b>Product:</b> JESSIE THUNDER SHOES
+                  </ProductName>
+                  <ProductId>
+                    <b>ID:</b> 93813718293
+                  </ProductId>
+                  <ProductColor color="black" />
+                  <ProductSize>
+                    <b>Size:</b>
+                    37.5
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  add
+                  <ProductAmount>2</ProductAmount>
+                  remove
+                </ProductAmountContainer>
+                <ProductPrice>$ 30</ProductPrice>
+              </PriceDetail>
+            </Product>
+          </Info>
+          <Summary>summary</Summary>
+        </Bottom>
       </Wrapper>
       <Footer />
     </Container>
